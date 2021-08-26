2e1cdeba35dee729b77ea2649114ea4e4f00035b
Liam Barkley <liambarkley@gmail.com>
2021-08-26 11:47:23 -0700

    clair-scanner v12

--- Formula/clair-scanner.rb
+++ Formula/clair-scanner.rb
@@ -9,16 +9,6 @@ class ClairScanner < Formula
     bin.install "clair-scanner_darwin_amd64" => "clair-scanner"
   end
 
-  def caveats; <<~EOS
-    👉 Note!!
-    This is not the official Formula from the creator of `clair-scanner`. It is only for my personal
-    use until Armin Coralic provides a official Formula.
-    In the meantime, jump over to github and star the repository.
-    https://github.com/arminc/clair-scanner
-    Kudos to Armin Coralic for creating this tool. 👍
-  EOS
-  end
-
   test do
     system "#{bin}/clair-scanner", "-h"
   end
